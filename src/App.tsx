575d3685ee037173ba215bd0d9bbfe40786e4f38
Maxat Nukhayev <maxat.nukhayev@gmail.com>
2022-03-26 08:10:48 +0100

    cleaned up CRA boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import "./App.css";
-
 const App = () => {
   return (
-    <h1 className="text-3xl bg-yellow-500 font-bold underline">Hello world!</h1>
+    <h1 className="text-3xl bg-green-500 font-bold underline">Hello world!</h1>
   );
 };
 
